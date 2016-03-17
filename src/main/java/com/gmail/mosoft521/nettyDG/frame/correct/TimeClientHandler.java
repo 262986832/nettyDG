4ab21737f54be03ce90a61470107adbaddb7e789
Alvin Zhang <mosoft521@gmail.com>
2016-03-17 17:04:57 +0800

    Netty TCP粘包问题之解决

--- src/main/java/com/gmail/mosoft521/nettyDG/frame/correct/TimeClientHandler.java
+++ src/main/java/com/gmail/mosoft521/nettyDG/frame/correct/TimeClientHandler.java
@@ -9,8 +9,7 @@ import java.util.logging.Logger;
 
 public class TimeClientHandler extends ChannelHandlerAdapter {
 
-    private static final Logger logger = Logger
-            .getLogger(TimeClientHandler.class.getName());
+    private static final Logger logger = Logger.getLogger(TimeClientHandler.class.getName());
 
     private int counter;
 
@@ -35,18 +34,15 @@ public class TimeClientHandler extends ChannelHandlerAdapter {
     }
 
     @Override
-    public void channelRead(ChannelHandlerContext ctx, Object msg)
-            throws Exception {
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
         String body = (String) msg;
-        System.out.println("Now is : " + body + " ; the counter is : "
-                + ++counter);
+        System.out.println("Now is : " + body + " ; the counter is : " + ++counter);
     }
 
     @Override
     public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
         // 释放资源
-        logger.warning("Unexpected exception from downstream : "
-                + cause.getMessage());
+        logger.warning("Unexpected exception from downstream : " + cause.getMessage());
         ctx.close();
     }
 }
--- src/main/java/com/gmail/mosoft521/nettyDG/frame/correct/TimeServerHandler.java
+++ src/main/java/com/gmail/mosoft521/nettyDG/frame/correct/TimeServerHandler.java
@@ -10,13 +10,10 @@ public class TimeServerHandler extends ChannelHandlerAdapter {
     private int counter;
 
     @Override
-    public void channelRead(ChannelHandlerContext ctx, Object msg)
-            throws Exception {
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
         String body = (String) msg;
-        System.out.println("The time server receive order : " + body
-                + " ; the counter is : " + ++counter);
-        String currentTime = "QUERY TIME ORDER".equalsIgnoreCase(body) ? new java.util.Date(
-                System.currentTimeMillis()).toString() : "BAD ORDER";
+        System.out.println("The time server receive order : " + body + " ; the counter is : " + ++counter);
+        String currentTime = "QUERY TIME ORDER".equalsIgnoreCase(body) ? new java.util.Date(System.currentTimeMillis()).toString() : "BAD ORDER";
         currentTime = currentTime + System.getProperty("line.separator");
         ByteBuf resp = Unpooled.copiedBuffer(currentTime.getBytes());
         ctx.writeAndFlush(resp);
